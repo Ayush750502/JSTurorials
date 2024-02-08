13534dd8dcd0141782714642e516fded4694e00d
Ayush750502 <103638475+Ayush750502@users.noreply.github.com>
2024-02-08 15:00:21 +0000

    Learned about variables in javascript

---
+++ variables.js
@@ -0,0 +1,20 @@
+const accId = 14456;
+// in-mutable variable'or constant'
+
+var accMail = "ninja@mail.com";
+/* 
+Prefer not to use it because js compiler uses var 
+as a globale decleration in all scopes
+*/
+let accPass = '12345';
+/*
+Prefered to use let to declare variables
+*/
+accCity = "HiddenVillage";
+/*
+Prefered not to used
+*/
+let accState;// = undefined
+
+console.table([accId , accMail , accPass , accCity , accState]);
+//              0       1           2        3          4
