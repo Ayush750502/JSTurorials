69b6f36e7c4c4fc06f4212a2083c7c8e43824308
Ayush750502 <ayush750502@gmail.com>
2024-02-09 22:30:13 +0530

    creating a pattern javascript code

---
+++ pattern01.js
@@ -0,0 +1,12 @@
+
+function pattern01(){
+    for(let i = 1 ; i <= 5 ; i++){
+        let out = "" ;
+        for(let j = 1 ; j <=i ; j++){
+            out += i;
+        }
+        console.log(out);
+    }
+}
+
+pattern01();
