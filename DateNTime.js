8d7a2046ded2c5b10cf7c88403dbfd32b1790cf1
Ayush750502 <ayush750502@gmail.com>
2024-02-14 10:46:37 +0530

    Learing About date n time in javascript

--- DateNTime.js
+++ DateNTime.js
@@ -17,6 +17,31 @@
 //console.log("New Date Object value(without time parameter values):-"+newDate.toLocaleString());
 //console.log("New Date Object value(with time parameter values):-"+newDate2.toLocaleString());
 
+//let newDate3 = new Date("23-01-2023");//Invalid Date
+//newDate3 = new Date("2023-01-23");
+//console.log(newDate3);
 
-let newDate3 = new Date("2023-01-23");
-console.log(newDate3);
+//let newDate4 = Date.now();
+//console.log(newDate4);// date in mili second
+//
+//console.log(Math.floor(newDate4/1000))//in seconds
+
+let newDate5 = new Date();
+//
+//console.log('Time: '+newDate5.getTime());//in miliseconds
+//console.log("Month: "+(newDate5.getMonth()+1));
+//console.log("Date: "+newDate5.getDate());
+//console.log("Day: "+newDate5.getDay());
+//Sun - 0
+//Mon - 1
+//Tue - 2
+//Wed - 3
+//Thur - 4
+//Fri - 5
+//Sat - 6
+console.log(newDate5);
+newDate5.toLocaleString('default' , {
+    weekday : 'long',
+});
+
+console.log(newDate5)
