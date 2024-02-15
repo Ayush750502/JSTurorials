77a70d911bfccc95cd47fecf41c9c9bb6ca88264
Ayush750502 <ayush750502@gmail.com>
2024-02-15 22:30:24 +0530

    adding new features to http page

--- http_02.js
+++ http_02.js
@@ -1,24 +1,38 @@
+const inpt = require('./selfModules/extractInput');
 const hwl = require('./selfModules/text');
 const http = require('node:http');//calling http module
+const url = require('node:url');//for taking url
 const hostname = '127.0.0.1';
 const port = 8000;
 
-const server = http.createServer((req , res) =>{//Calling New Server
-    console.log('Page is loaded!');
+http.createServer(function (req , res){//Calling New Server
     //res.statusCode = 200;//res -> response object
     ////Here it is sending a stutus code telling that request is fulfilled
     //res.setHeader('Content-Type' , 'text/plain');
     ////Used to set a single header value
     ////Or we can use
     res.writeHead(200 , 'Content-Type' , 'text/html')
-    res.write(hwl.htmltext());
+    res.write(hwl.htmltext(port));
+    console.log('Page is loaded!');
+    let link = url.parse(req.url , true);
+    search = link.search;
+    let ins = inpt.giveInputs(search);
+    //console.log(`the value received: '${ins}' \nit's type: ${typeof ins}`);
+    console.log("value:\n");
+    if(ins != null){
+        ins.forEach(element => {
+            console.log(element);
+        });
+    }
+    else{
+        console.log('It is null');
+    }
+    console.log(`It's type is ${typeof ins}`);
     console.log('server is ending');
     res.end();
-});
-
-server.listen(port , () => {
+}).listen(port , function (){
     console.log(`Server is running at -
-    
+
     'http://localhost:${port}'
                  or
     'http://${hostname}:${port}' `);
--- selfModules/text.js
+++ selfModules/text.js
@@ -1,8 +1,15 @@
 
-exports.htmltext = function(){
+exports.htmltext = function(port){
     let hwl = `
 <div>
     <h1>Hello World!</h1>
+    <form action='http://127.0.0.1:${port}'>
+    <label for='value'>Enter some value:- </label>
+    <input name='value1' placeholder='bla-bla-bla' value='bla-bla-bla'><br>
+    <label for='value'>Enter some value:- </label>
+    <input name='value2' placeholder='bla-bla-bla' value='bla-bla-bla'><br>
+    <button type="submit">Submit your answer!</button>
+    </form>
 </div>
 `;
     return hwl;
