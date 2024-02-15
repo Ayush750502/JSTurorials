5d6c3a1b2285ba045768aae772646c5d6f5a6543
Ayush750502 <ayush750502@gmail.com>
2024-02-15 19:23:50 +0530

    understanding about node,js http server

--- http_02.js
+++ http_02.js
@@ -1,10 +1,21 @@
+const hwl = require('./selfModules/text');
 const http = require('node:http');//calling http module
 const hostname = '127.0.0.1';
 const port = 8000;
 
 const server = http.createServer((req , res) =>{//Calling New Server
-    res.statusCode = 200;//res -> response object
-    //Here it is sending a stutus code telling that request is fulfilled
-    res.setHeader('Content-Type' , 'text/plain');
-    res.end('Hello World!\n');
+    console.log('Page is loaded!');
+    //res.statusCode = 200;//res -> response object
+    ////Here it is sending a stutus code telling that request is fulfilled
+    //res.setHeader('Content-Type' , 'text/plain');
+    ////Used to set a single header value
+    ////Or we can use
+    res.writeHead(200 , 'Content-Type' , 'text/html')
+    res.write(hwl.htmltext());
+    console.log('server is ending');
+    res.end();
+});
+
+server.listen(port , () => {
+    console.log(`Server is running at -\n\t'http://localhost:${port}'\n \t\tor\n\t'http://${hostname}:${port}' `);
 });
