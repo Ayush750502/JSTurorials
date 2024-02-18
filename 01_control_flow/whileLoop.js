c9dd253a29a912733b68bcc6591214c330ab9fc3
Ayush750502 <ayush750502@gmail.com>
2024-02-18 14:55:46 +0530

    learning  about while loop in JavaScript

---
+++ 01_control_flow/whileLoop.js
@@ -0,0 +1,18 @@
+//// while loop:-
+let i = 0;
+while(true){
+    console.log("run");
+    if(i === 5){
+        break;
+    }
+    i++;
+}
+
+//      or
+
+i = 0;
+while (i != 5) {
+    console.log("running");
+    i++;
+}
+
